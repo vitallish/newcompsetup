016a32f50dd92bfca29520859b6231ec7e996a3c
vitallish <vdruker@gmail.com>
2019-03-12 07:40:51 -0400

    added dependency for tidy and tex

--- ubunutu1804.sh
+++ ubunutu1804.sh
@@ -6,10 +6,15 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
 sudo apt-get update
 
 # install R
-sudo apt-get install r-base
+sudo apt-get install -y r-base
 
 # install R Studio Server
 sudo apt-get install gdebi-core
 wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-1.2.1322-amd64.deb
 sudo gdebi rstudio-server-1.2.1322-amd64.deb
 
+# R dependencies
+# for xml
+sudo apt-get install -y libcurl4-openssl-dev libssl-dev libxml2-dev
+# tex
+sudo apt install -y texlive-latex-extra
